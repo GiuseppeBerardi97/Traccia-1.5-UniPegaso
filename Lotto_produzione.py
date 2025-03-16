9efa914c54ebf08eeb350ccde020dfc9e743cec6
GiuseppeBerardi97 <gberardi745@gmail.com>
2025-03-16 13:34:03 +0100

    Creazione funzione per conversione tempo in  mintui, stampa output

--- Lotto_produzione.py
+++ Lotto_produzione.py
@@ -31,3 +31,31 @@ def genera_parametri_produzione():
     somma_capacita_massima = sum(parametri[prodotto]["capacita_massima_giornaliera"] for prodotto in parametri if prodotto != "Capacita_Complessiva")
     
     return parametri, somma_capacita_massima
+
+# Funzione per calcolare il tempo totale di produzione in minuti
+def calcola_tempo_totale_produzione(quantita, parametri):
+    return sum(quantita[prodotto] * parametri[prodotto]["tempo_per_unita"] for prodotto in quantita)
+
+if __name__ == "__main__":
+    quantita = genera_quantita_produzione()
+    parametri, somma_capacita_massima = genera_parametri_produzione()
+    
+    print("Quantità giornaliera richiesta da produrre:")
+    for prodotto, qta in quantita.items():
+        print(f"{prodotto}: {qta}")
+    
+    print("\nParametri di produzione (tempo in minuti):")
+    for prodotto, param in parametri.items():
+        if prodotto != "Capacita_Complessiva":
+            print(f"{prodotto} - Tempo per unità: {param['tempo_per_unita']} min, Capacita massima giornaliera: {param['capacita_massima_giornaliera']}")
+        else:
+            print(f"\nCapacita complessiva giornaliera: {param} minuti (massimo 24 ore)")
+
+    print(f"\nSomma delle capacità massime giornaliere dei prodotti: {somma_capacita_massima} unità")
+
+    tempo_totale_minuti = calcola_tempo_totale_produzione(quantita, parametri)
+    
+    print("\nRiepilogo della produzione:")
+    for prodotto in quantita:
+        tempo_prodotto_minuti = quantita[prodotto] * parametri[prodotto]['tempo_per_unita']
+        print(f"{prodotto}: {quantita[prodotto]} unità x {parametri[prodotto]['tempo_per_unita']} min per unità = {tempo_prodotto_minuti} minuti")
